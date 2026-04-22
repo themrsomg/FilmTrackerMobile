daa61d238f210050fe72e7b8e10c6af54b45b310
themrsomg <themrsomg117@gmail.com>
2026-04-21 20:12:59 -0600

    feat: Se añadieron endpoints referentes a la interacción API-Cliente

--- app/src/main/java/com/example/santabarbaramobile/data/remote/SantaBarbaraApi.kt
+++ app/src/main/java/com/example/santabarbaramobile/data/remote/SantaBarbaraApi.kt
@@ -1,7 +1,13 @@
 package com.example.santabarbaramobile.data.remote
 
 import com.example.santabarbaramobile.data.model.HomeResponse
+import com.example.santabarbaramobile.data.model.Season
 import com.example.santabarbaramobile.data.model.Show
+import com.example.santabarbaramobile.data.model.Episode
+import com.example.santabarbaramobile.data.model.CastMember
+import com.example.santabarbaramobile.data.model.CastResponse
+import com.example.santabarbaramobile.data.model.EpisodeResponse
+import com.example.santabarbaramobile.data.model.SeasonResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.Response
@@ -12,4 +18,13 @@ interface SantaBarbaraApi {
 
     @GET("api/shows/{id}")
     suspend fun getShowById(@Path("id") showId: String): Response<Show>
+
+    @GET("api/shows/{id}/seasons")
+    suspend fun getShowSeasons(@Path("id") showId: String): Response<SeasonResponse>
+
+    @GET("api/shows/{id}/episodes")
+    suspend fun getShowEpisodes(@Path("id") showId: String): Response<EpisodeResponse>
+
+    @GET("api/shows/{id}/cast")
+    suspend fun getShowCast(@Path("id") showId: String): Response<CastResponse>
 }
