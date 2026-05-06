f4459bb220201c95ebedaa73d57b9d2a0cc5879d
themrsomg <themrsomg117@gmail.com>
2026-05-06 08:33:27 -0600

    refactor: Cambios para solucionar problemática con funcionalidad "Mi perfil" y conectividad entre backend y frontend

--- app/src/main/java/com/example/santabarbaramobile/di/NetworkModule.kt
+++ app/src/main/java/com/example/santabarbaramobile/di/NetworkModule.kt
@@ -1,12 +1,15 @@
 package com.example.santabarbaramobile.di
 
+import android.util.Log
 import com.example.santabarbaramobile.data.remote.SantaBarbaraApi
 import com.example.santabarbaramobile.data.remote.auth.AuthApi
+import com.example.santabarbaramobile.data.remote.users.UsersApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -21,17 +24,51 @@ annotation class ShowsRetrofit
 @Retention(AnnotationRetention.BINARY)
 annotation class AuthRetrofit
 
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class UsersRetrofit
+
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
     private const val SHOWS_BASE_URL = "http://10.0.2.2:3001/"
     private const val AUTH_BASE_URL = "http://10.0.2.2:3003/"
+    private const val USERS_BASE_URL = "http://10.0.2.2:3002/"
+
 
     @Provides
     @Singleton
-    fun provideOkHttpClient(): OkHttpClient {
+    @UsersRetrofit
+    fun provideUsersRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(USERS_BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideUsersApi(@UsersRetrofit retrofit: Retrofit): UsersApi {
+        return retrofit.create(UsersApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        val interceptor = HttpLoggingInterceptor { message ->
+            Log.d("RetrofitTraffic", message)
+        }
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return interceptor
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
         return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
             .connectTimeout(15, TimeUnit.SECONDS)
             .readTimeout(15, TimeUnit.SECONDS)
             .writeTimeout(15, TimeUnit.SECONDS)
